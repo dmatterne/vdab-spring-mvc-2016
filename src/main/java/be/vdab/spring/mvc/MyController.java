93b5a2ab9c406809c4626d47b53df3ccec645671
Kevin Van Robbroeck <kevin.van.robbroeck@gmail.com>
2016-09-02 15:06:21 +0200

    Added sample of creation form

--- src/main/java/be/vdab/spring/mvc/MyController.java
+++ src/main/java/be/vdab/spring/mvc/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -33,5 +34,10 @@ public class MyController {
     public String ex(Model model) {
         model.addAttribute("filmList", fr.findAll());
         return "films";
+    }
+
+    @RequestMapping("/addfilm")
+    public String addFilm() {
+        return "addfilm";
     }
 }
